c81123b392fa615a3b6c8bd4e7164250cbbf1f3e
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@users.sourceforge.net>
2025-01-12 10:43:16 +0100

    Renamed wrapper scripts to be used as git subcommands

--- git-glab-pipe.sh
+++ git-glab-pipe.sh
@@ -2,16 +2,15 @@
 
 # TODO:
 #   check args for cfg file
-#   set as git cmd
 if [ "x$1" = "x--git-remote" ]; then
   git_repo="$1 $2"
   shift 2
 fi
 # last pipeline:
-pipeid=$(gitlab_wrapper.sh $git_repo project-pipeline list | head -n 1 | cut -d: -f2)
-gitlab_wrapper.sh $git_repo -o yaml -f id,status,web_url project-pipeline get --id $pipeid
+pipeid=$(git gitlab $git_repo project-pipeline list | head -n 1 | cut -d: -f2)
+git gitlab $git_repo -o yaml -f id,status,web_url project-pipeline get --id $pipeid
 
-gitlab_wrapper.sh $git_repo -o yaml -f id,name,status,web_url project-pipeline-job list --pipeline-id $pipeid
+git gitlab $git_repo -o yaml -f id,name,status,web_url project-pipeline-job list --pipeline-id $pipeid
 
 #projurl=$(echo $url | sed "s#:\($proj\)#/\\1#")
 #projurl=$(echo $projurl | sed 's#git@#https://#')
