1504cd4eb5d99a583330432741040034798fd3b4
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@users.sourceforge.net>
2025-01-14 21:10:55 +0100

    git-glab-pipe: check that $pipeid is not empty before fetching jobs

--- git-glab-pipe.sh
+++ git-glab-pipe.sh
@@ -8,6 +8,7 @@ if [ "x$1" = "x--remote" ]; then
 fi
 # last pipeline:
 pipeid=$(git gitlab $git_repo project-pipeline list | head -n 1 | cut -d: -f2)
+[ -n "$pipeid" ] || exit 0
 git gitlab $git_repo -o yaml -f id,status,web_url project-pipeline get --id $pipeid
 
 git gitlab $git_repo -o yaml -f id,name,status,web_url project-pipeline-job list --pipeline-id $pipeid
